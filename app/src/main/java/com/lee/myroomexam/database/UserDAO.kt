29e650f501c88adf74adc7f75830711e114973a0
LAPTOP-9PA10K2Q\이원곽 <bacassf@gmail.com>
2023-06-16 16:11:57 +0900

    feat : library update.

--- app/src/main/java/com/lee/myroomexam/database/UserDAO.kt
+++ app/src/main/java/com/lee/myroomexam/database/UserDAO.kt
@@ -3,7 +3,7 @@ package com.lee.myroomexam.database
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
 
 @Dao
